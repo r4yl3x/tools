8a10048c5f331ea274d78163204e99370c67cf7c
RAYLEX <78780967+r4yl3x@users.noreply.github.com>
2022-12-28 12:47:49 +0100

    Update ipbyhost2.py
    Added argument -o to specify the output file
    Added error messages

--- ipbyhost2.py
+++ ipbyhost2.py
@@ -1,50 +1,38 @@
 import argparse
 import socket
 
-# Define a function to resolve the IP addresses for a list of domain names
-def get_ip_addresses(domains):
-    # Create a list to store the results
-    results = []
+# Set up the argument parser
+parser = argparse.ArgumentParser(description='Resolve IP addresses for a list of domain names')
+parser.add_argument('-f', '--file', help='input file containing a list of domain names, one per line')
+parser.add_argument('-d', '--domains', nargs='+', help='list of domain names')
+parser.add_argument('-o', '--output', help='output file to store the results')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Read in the list of domain names
+if args.file:
+    # Read the domain names from the input file
+    with open(args.file, 'r') as f:
+        domains = f.read().splitlines()
+elif args.domains:
+    # Use the list of domain names provided as an argument
+    domains = args.domains
+else:
+    print('Error: no input provided')
+    exit(1)
+
+# Open the output file for writing
+with open(args.output, 'w') as f:
     # Iterate over the list of domain names
     for domain in domains:
         # Try to resolve the IP address for the domain name
         try:
             ip = socket.gethostbyname(domain)
-            # Add the domain name and IP address to the results list
-            results.append((domain, ip))
+            # Write the domain name and IP address to the output file
+            f.write(f'{domain},{ip}\n')
+            print(f'{domain},{ip}')
         except socket.gaierror:
-            # If the domain name could not be resolved, add an error message to the results list
-            results.append((domain, 'ERROR'))
-    return results
-
-# Create a parser object to handle command-line arguments
-parser = argparse.ArgumentParser(description='Resolve IP addresses for a list of domain names')
-
-# Add arguments to the parser
-parser.add_argument('-f', '--file', help='a file containing a list of domain names, one per line')
-parser.add_argument('-d', '--domains', nargs='+', help='a list of domain names')
-
-# Parse the command-line arguments
-args = parser.parse_args()
-
-# Check if the user specified a file or a list of domain names
-if args.file:
-    # Read in the list of domain names from the specified file
-    with open(args.file, 'r') as f:
-        domains = f.read().splitlines()
-else:
-    # Use the list of domain names provided as an argument
-    domains = args.domains
-
-# Resolve the IP addresses for the list of domain names
-results = get_ip_addresses(domains)
-
-# Open the output file for writing
-with open('iplist.txt', 'w') as f:
-    # Iterate over the results and write them to the output file
-    for domain, ip in results:
-        f.write(f'{domain},{ip}\n')
-
-# Print the results to the console
-for domain, ip in results:
-    print(f'{domain}: {ip}')
+            # If the domain name could not be resolved, write an error message to the output file
+            f.write(f'{domain},ERROR\n')
+            print(f'{domain},ERROR')
